68c0f1d3137b4f878d7df59c128a93d1e41041cf
dbosst <dbosst@gmail.com>
2024-09-05 09:07:31 -0400

    Forgot to overwrite updated fix for compare csv

--- tool/compare_csv.py
+++ tool/compare_csv.py
@@ -1,4 +1,3 @@
-# generates a comparison between csv files (for global monthly means csv)
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
@@ -21,19 +20,26 @@ df2 = pd.read_csv(sys.argv[2], skiprows=2, usecols=range(1, 13))
 df1 = df1.apply(pd.to_numeric, errors='coerce')
 df2 = df2.apply(pd.to_numeric, errors='coerce')
 
-# Calculate the absolute differences between the two files
-diff_values = (df1 - df2).abs().stack()
+diff_values = []
+for (index1, row1), (index2, row2) in zip(df1.iterrows(), df2.iterrows()):
+    for col in range(len(row1)):
+        if index1 == index2:  # Ensure corresponding rows
+            # make sure 2 decimal precision (as gistemp is only 2 decimals)
+            diff = round(abs(row1[col] - row2[col]), 2)
+            if not np.isnan(diff):
+                if diff > 0.01:
+                    print(diff)
+                diff_values.append(diff)
 
+diff_values = pd.Series(diff_values)
 total_diff = diff_values[diff_values > 0].count()
 
+
 # Create a histogram with a bin size of 0.01
-plt.hist(diff_values[diff_values > 0], bins=np.arange(0, diff_values.max() + 0.01, 0.01), edgecolor='black')
+plt.hist(diff_values[diff_values > 0], bins=np.arange(0, diff_values.max() + 0.01, 0.001), edgecolor='black')
 plt.xlabel('Absolute Difference')
 plt.ylabel('Count')
 plt.title(f'Histogram of Absolute Differences (Total Differences: {total_diff}){subtitle}')
 
-
 # Save the figure to a PNG file
 plt.savefig(f'compare_csv_histogram.png', bbox_inches='tight')
-
-print(f'Figure saved to compare_csv_histogram.png')
